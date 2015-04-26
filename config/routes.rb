978298eebb6079e1ff8282de0b96d8ed56c46cef
Sean Tsai <kidd826@gmail.com>
2015-04-26 21:01:17 +0800

    Add comment function

--- config/routes.rb
+++ config/routes.rb
@@ -4,7 +4,9 @@ Rails.application.routes.draw do
   # See how all your routes lay out with "rake routes".
 
   # You can have the root of your site routed with "root"
-  resources :photos
+  resources :photos do
+    resources :comments
+  end
   #root :to => 'welcome#index'
   root :to => 'photos#index'
 
